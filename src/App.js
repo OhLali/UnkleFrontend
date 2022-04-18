7f26bab49a61446dee5b35d47d5e82a67d347b9c
LaureLine <laureline.rogerieux@gmail.com>
2022-04-18 20:00:06 +0200

    composant de base créé

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Tooltip from './component/tooltip'
+import TooltipToDisplay from './component/tooltip'
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Tooltip></Tooltip>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TooltipToDisplay></TooltipToDisplay>
+        
       </header>
+      
     </div>
   );
 }
--- src/component/tooltip.js
+++ src/component/tooltip.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Tooltip, Whisper } from 'rsuite';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+//import 'rsuite/styles/index.less';
+import "rsuite/dist/rsuite.min.css";
+import { Button } from 'reactstrap'
 
-function Tootip() {
+function TooltipToDisplay() {
+
+    
+    let colorInfo = {color: '#2b9dfe',cursor:'pointer'}
+
+    const tooltip = (
+        <Tooltip >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Tooltip>
+      ); 
+    
+    
+    let iconInfo = (<FontAwesomeIcon icon={faCircleInfo} style={colorInfo} />)
 
     return (
-        <p>Hello</p>
+        <div>
+            <div style={{
+                display: 'block', width: 700, paddingLeft: 30
+            }}>
+               
+                <Whisper placement="rightStart" trigger="hover" speaker={tooltip}>
+                    <Button className="buttonTooltipInfo" ><FontAwesomeIcon icon={faCircleInfo} style={colorInfo} /></Button>
+                </Whisper>
+
+            </div>
+          
+        </div>   
+        
     )
 
 }
 
-export default Tootip
+export default TooltipToDisplay
